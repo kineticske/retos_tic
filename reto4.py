0d2aa09b60ab565c355867a1f7b5a2d08118c316
Kineticske <karinaestherarjonariano@gmail.com>
2022-05-31 16:46:49 -0500

    separador comas

--- reto4.py
+++ reto4.py
@@ -1,10 +1,4 @@
 from functools import reduce
-rutinaContable=[ 
- [6589, ("9874", 1, 125698.20), ("88112", 2, 135645.20), ("3218", 4, 13645.20)],
- [6590, ("5236", 8, 11.99), ("7733",11,18.99), ("88112", 5, 390.95)],
- [6591, ("7812", 2, 11.99), ("9652",11,18.99)]
-]
-
 def ordenes(rutinaContable):
     print('----------------- Inicio Registro diario --------------------------')
     
@@ -16,7 +10,6 @@ def ordenes(rutinaContable):
         total_pesos=reduce(lambda x,y: x+y, lista_precios)
         if (total_pesos<600000):
             total_pesos+=25000
-        print(f'La factura {codigo_ref} tiene un total en pesos de {total_pesos:.2f}')
+        print(f'La factura {codigo_ref} tiene un total en pesos de {total_pesos:,.2f}')
     print('----------------- Fin Registro diario -----------------------------')
 
-ordenes(rutinaContable)
